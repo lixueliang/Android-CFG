eeab89075a5ead6874a799f46a7b75ee86204183
lixueliang <xueliang@cbit-pc645.local>
2016-04-15 11:26:17 +0200

    Add more comments and polished
    as title said

--- src/org/CFG/CFGraph.java
+++ src/org/CFG/CFGraph.java
@@ -17,6 +17,7 @@ import soot.SootMethod;
 //import soot.jimple.infoflow.android.SetupApplication;
 
 import soot.Unit;
+import soot.ValueBox;
 import soot.jimple.infoflow.android.SetupApplication;
 import soot.jimple.spark.SparkTransformer;
 import soot.options.Options;
@@ -48,6 +49,8 @@ public class CFGraph {
 	   // TODO Auto-generated catch block
 	      e.printStackTrace();
        }
+	   
+	   // Initial settings
        soot.G.reset();
 	   Options.v().set_src_prec(Options.src_prec_apk);
 	   Options.v().set_process_dir(Collections.singletonList("/Users/xueliang/Documents/tools/AndroidInstrument/cocos2d_android.apk"));
@@ -56,58 +59,72 @@ public class CFGraph {
 	   Options.v().set_allow_phantom_refs(true);
 	   Options.v().set_output_format(Options.output_format_none);
 	   Options.v().setPhaseOption("cg.spark", "on");
-	   //Options.v().set_output_dir("/Users/xueliang/Documents/SootOutput");
-       //Options.v().interactive_mode();
-       //    HashMap<String,String> opt = new HashMap<String,String>();
-       //   opt.put("verbose", "true");
-       //   SparkTransformer.v().transform("cg", opt);;
+	   
+       // Load all the classes
        Scene.v().loadNecessaryClasses(); 
+       
+       // Creat a dummy main method
        SootMethod entryPoint = app.getEntryPointCreator().createDummyMain();
        Options.v().set_main_class(entryPoint.getSignature());
        Scene.v().setEntryPoints(Collections.singletonList(entryPoint));
 	   /////System.out.println(entryPoint.getActiveBody());
        PackManager.v().runPacks();
 	   /////System.out.println(Scene.v().getCallGraph());
+       
+       //Traverse all the classes
        Iterator<SootClass> cit = Scene.v().getApplicationClasses().iterator();
        while (cit.hasNext()){
     	   // Set up the class we are working with
     	   SootClass c = (SootClass) cit.next();
-    	   printCFG_basedOn_id(c);
+    	   printCFG_basedOn_string(c);
        }
     }
 	
 	
 	public static void printCFG_basedOn_id(SootClass c){
-		String className = c.getName();
+	   String className = c.getName();
+	   
+	   //Traverse all the methods
  	   Iterator<SootMethod> mit =  c.methodIterator();    	   
  	   while (mit.hasNext()){
  		   //Retrieve method and its body
  		   SootMethod m = mit.next();
  		   String methodName = m.getName();
  		   /////System.out.println(className+"."+methodName);
- 		   ///*
  		   try{
  		   Body b = m.retrieveActiveBody();
- 		   //Build the CFG
- 		  ExceptionalUnitGraph g = new ExceptionalUnitGraph(b);
+ 		   
+ 		   // Build the CFG
+ 		   ExceptionalUnitGraph g = new ExceptionalUnitGraph(b);
  		   /////System.out.println("methodCFG: " + className+"."+methodName+"\n\n"+ g.toString());
  		   /////Unit unit = (Unit) g.iterator();
  		   /////System.out.println("methodCFG: "+ g.toString());
- 		  System.out.println("\nmethodCFG: " + className+"."+methodName+"\n");
- 		  System.out.println("digraph mygraph{");
- 		   /***********************Process units in the Method****************************/
+ 		   
+ 		   // Print out the instructions needed for Graphviz to draw graphs
+ 		   System.out.println("\nmethodCFG: " + className+"."+methodName+"\n");
+ 		   System.out.println("digraph mygraph{");
+ 		   
+ 		   // Traverse all the units
  		   Iterator<Unit> uit = g.iterator();
  	       while(uit.hasNext()){
  		        Unit u = uit.next();
  		        int uid = u.hashCode();
+ 		        
+ 		        // Obtain the unexceptional successes 
  		        List<Unit> unexcp_seccs = g.getUnexceptionalSuccsOf(u);
  		        for(Unit secc : unexcp_seccs){
  		        	int seccid = secc.hashCode();
+ 		        	
+ 		        	// Print out the directed edge 
  		        	System.out.println("   "+uid+" -> "+seccid+";");
  		        }
+ 		        
+ 		        // Obtain the exceptional successes
  		        List<Unit> excp_seccs = g.getExceptionalSuccsOf(u);
- 		       for(Unit secc : excp_seccs){
+ 		        for(Unit secc : excp_seccs){
 		        	int seccid = secc.hashCode();
+		        	
+		        	// Print out the directed edge
 		        	System.out.println("   "+uid+" -> "+seccid+" [style=dotted];");
 		        }
  		        
@@ -116,12 +133,8 @@ public class CFGraph {
  		   
  		   } catch(RuntimeException e)
  		   {e.printStackTrace();}
- 		   //*/
  	   }
 	}
-	
-	
-	
 
 
 }
